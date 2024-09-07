6147b1d5b5dea554bbefd2186e699950851c21b4
KevinMitsi <kevinandresgarciaaguirre@gmail.com>
2024-09-07 18:42:11 -0500

    HOT FIX
    se corrige una falta de constantes en el UserController

--- src/main/java/com/kevin/emazon_users/infraestructure/controller/UserController.java
+++ src/main/java/com/kevin/emazon_users/infraestructure/controller/UserController.java
@@ -8,24 +8,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Validated
 @RequestMapping("api/v1/user")
 @AllArgsConstructor
 public class UserController {
-    public static final String ROLE_ADMINISTRADOR_KEY = "ROLE_ADMINISTRADOR";
+    public static final String ROLE_ADMINISTRATOR = "ROLE_ADMINISTRADOR";
     public static final String CREATED_ADMIN_CONFIRM_MESSAGE = "Felicidades ha creado el admin: ";
     public static final String CREATED_WH_CONFIRMATION_MESSAGE = "Felicidades ha creado el Auxiliar: ";
 
 
     private final IUserHandler userHandler;
     @PostMapping("/register")
-    @Secured(ROLE_ADMINISTRADOR_KEY)
+    @Secured(ROLE_ADMINISTRATOR)
     public ResponseEntity<String> createAdmin(@Valid @RequestBody UserDto userDto){
         userHandler.saveUser(userDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(CREATED_ADMIN_CONFIRM_MESSAGE + userDto.getName());
@@ -33,7 +30,7 @@ public class UserController {
 
 
     @PostMapping("/register/whWorker")
-    @Secured(ROLE_ADMINISTRADOR_KEY)
+    @Secured(ROLE_ADMINISTRATOR)
     public ResponseEntity<String> registerNewUser(@Valid @RequestBody UserDto userDto){
         userHandler.saveUser(userDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(CREATED_WH_CONFIRMATION_MESSAGE + userDto.getName());
